6efa74f90565a6ce113cb96737b8ed29086c5e7e
JOSE JHAIR HERNANDEZ TSEREMP <jj.hernandezt@uea.edu.ec>
2025-08-19 19:43:38 -0500

    Semana 10

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return '¡Hola! Bienvenido a mi aplicación Flask.'
+@app.route("/")
+def home():
+    return render_template("index.html", title="Inicio")
 
-@app.route('/usuario/<nombre>')
-def usuario(nombre):
-    return f'Bienvenido, {nombre}!'
+@app.route("/about")
+def about():
+    return render_template("about.html", title="Acerca de")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
